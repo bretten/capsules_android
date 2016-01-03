7862b51cf431f375b7d79f6efcc28b50b4fd6495
bretten <brettinbox@gmail.com>
2016-01-02 21:00:19 -0800

    CapsuleListActivity no longer has two ListFragments that displays Capsules and Discoveries, now just has a ListView to display just Capsules.  Added SortDialogFragment to CapsuleListActivity

--- src/com/brettnamba/capsules/activities/CapsuleListActivity.java
+++ src/com/brettnamba/capsules/activities/CapsuleListActivity.java
@@ -1,28 +1,89 @@
 package com.brettnamba.capsules.activities;
 
 import android.accounts.Account;
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.brettnamba.capsules.R;
-import com.brettnamba.capsules.fragments.CapsuleListFragment;
-import com.brettnamba.capsules.provider.CapsuleContract;
+import com.brettnamba.capsules.dataaccess.Capsule;
+import com.brettnamba.capsules.fragments.SortDialogFragment;
+import com.brettnamba.capsules.http.CapsuleRequestParameters;
+import com.brettnamba.capsules.http.RequestHandler;
+import com.brettnamba.capsules.http.response.JsonResponse;
+import com.brettnamba.capsules.os.AsyncListenerTask;
+import com.brettnamba.capsules.os.GetCapsulesTask;
+import com.brettnamba.capsules.os.RetainedTaskFragment;
 import com.brettnamba.capsules.util.Widgets;
-import com.brettnamba.capsules.view.FragmentCollectionPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Activity that displays a user's Capsules and Discovery Capsules.
+ * Activity that displays a user's Capsules.
  *
  * @author Brett Namba
  */
 public class CapsuleListActivity extends FragmentActivity implements
-        CapsuleListFragment.CapsuleListFragmentListener {
+        AsyncListenerTask.GetCapsulesTaskListener,
+        SortDialogFragment.SortDialogListener {
+
+    /**
+     * The current Account
+     */
+    private Account mAccount;
+
+    /**
+     * Fragment used to retain background thread tasks
+     */
+    private RetainedTaskFragment mRetainingFragment;
+
+    /**
+     * The request parameters for retrieving Capsules from the Web API
+     */
+    private CapsuleRequestParameters mRequestParams;
+
+    /**
+     * Flag that determines if the ListView is loading more Capsule items due to being scrolled
+     * to the bottom of the list
+     */
+    private boolean mIsScrollLoading;
+
+    /**
+     * Adapter that loads the Capsule data
+     */
+    private ArrayAdapter<String> mAdapter;
+
+    /**
+     * Collection of Capsules that are displayed in the ListView
+     */
+    private ArrayList<Capsule> mCapsules;
+
+    /**
+     * Tag for storing the collection of Capsules in state data
+     */
+    private static final String TAG_CAPSULES = "capsules";
+
+    /**
+     * Tag for storing the request parameters in state data
+     */
+    private static final String TAG_REQUEST_PARAMS = "request_params";
+
+    /**
+     * Tag for the Fragment that retains background thread tasks
+     */
+    private static final String TAG_RETAINING_FRAGMENT = "retaining_fragment";
+
+    /**
+     * Tag for the Fragment that displays the sort options
+     */
+    private static final String TAG_SORT_DIALOG = "sort_dialog";
 
     /**
      * onCreate
@@ -34,75 +95,229 @@ public class CapsuleListActivity extends FragmentActivity implements
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_capsule_list);
 
-        // The current Account
-        Account account = null;
-
         // Get the Intent extras
         Bundle extras = this.getIntent().getExtras();
         if (extras != null) {
-            account = extras.getParcelable("account");
+            this.mAccount = extras.getParcelable("account");
         }
 
         // Close the Activity if the required data was not present in the extras
-        if (account == null) {
+        if (this.mAccount == null) {
             this.finish();
         }
 
-        // Bundle data to be passed to the Capsule List Fragment
-        Bundle ownershipBundle = new Bundle();
-        ownershipBundle.putBoolean("owned", true);
-        ownershipBundle.putParcelable("account", account);
-        ownershipBundle.putParcelable("uri", CapsuleContract.Ownerships.CONTENT_URI.buildUpon()
-                        .appendQueryParameter(CapsuleContract.Query.Parameters.INNER_JOIN, CapsuleContract.Capsules.TABLE_NAME)
-                        .build()
-        );
-        ownershipBundle.putStringArray("projection", CapsuleContract.Ownerships.CAPSULE_JOIN_PROJECTION);
-        ownershipBundle.putString("selection", CapsuleContract.Ownerships.ACCOUNT_NAME + " = ?");
-        ownershipBundle.putStringArray("selection_args", new String[]{account.name});
+        // Set up the Fragment that retains background thread tasks
+        this.setupRetainedFragment();
 
-        // Bundle the data to be passed to the Discovery List Fragment
-        Bundle discoveryBundle = new Bundle();
-        discoveryBundle.putBoolean("owned", false);
-        discoveryBundle.putParcelable("account", account);
-        discoveryBundle.putParcelable("uri", CapsuleContract.Discoveries.CONTENT_URI.buildUpon()
-                        .appendQueryParameter(CapsuleContract.Query.Parameters.INNER_JOIN, CapsuleContract.Capsules.TABLE_NAME)
-                        .build()
-        );
-        discoveryBundle.putStringArray("projection", CapsuleContract.Discoveries.CAPSULE_JOIN_PROJECTION);
-        discoveryBundle.putString("selection", CapsuleContract.Discoveries.ACCOUNT_NAME + " = ?");
-        discoveryBundle.putStringArray("selection_args", new String[]{account.name});
+        // Check if the Activity has any retained state data
+        if (savedInstanceState != null) {
+            // Get the retained request parameters
+            this.mRequestParams = savedInstanceState.getParcelable(TAG_REQUEST_PARAMS);
+            // Get the retained Capsules
+            this.mCapsules = savedInstanceState.getParcelableArrayList(TAG_CAPSULES);
+        } else {
+            // Instantiate the request parameters
+            this.mRequestParams = new CapsuleRequestParameters().setPage(1);
+            // Instantiate the Capsule collection
+            this.mCapsules = new ArrayList<Capsule>();
+            // Request the initial Capsules
+            this.requestCapsules(this.mRequestParams);
+        }
 
-        // Instantiate the Fragments
-        // Fragment that will hold the list of Ownerships
-        CapsuleListFragment ownershipFragment = new CapsuleListFragment();
-        ownershipFragment.setArguments(ownershipBundle);
-        // Fragment that will hold the list of Discoveries
-        CapsuleListFragment discoveryFragment = new CapsuleListFragment();
-        discoveryFragment.setArguments(discoveryBundle);
-
-        // Instantiate the PagerAdapter
-        FragmentCollectionPagerAdapter pagerAdapter =
-                new FragmentCollectionPagerAdapter(this.getSupportFragmentManager());
-        // Set the Fragments on the pager
-        pagerAdapter.addFragment(ownershipFragment)
-                .addFragment(discoveryFragment);
-
-        // Set up the tabs
-        TabLayout tabLayout = (TabLayout) this.findViewById(R.id.activity_capsule_list_tabs);
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.title_my_capsules));
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.title_my_discoveries));
-
-        // Get the ViewPager
-        ViewPager viewPager = (ViewPager) this.findViewById(R.id.fragment_capsule_list_pager);
-        // Set the PagerAdapter on the ViewPager
-        viewPager.setAdapter(pagerAdapter);
-        // Set the page change listener
-        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        // Add the OnTabSelectedListener implemented by the ViewPager
-        tabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+        // Setup the ListView
+        this.setupListView(this.mCapsules);
 
         // Setup the Toolbar
+        this.setupToolbar();
+    }
+
+    /**
+     * onSaveInstanceState
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the request parameters
+        outState.putParcelable(TAG_REQUEST_PARAMS, this.mRequestParams);
+        // Save the Capsule collection
+        outState.putParcelableArrayList(TAG_CAPSULES, this.mCapsules);
+    }
+
+    /**
+     * Requests Capsules from the server and parses the response
+     *
+     * @param params Parameters for the request
+     * @return The parsed collection of Capsules
+     */
+    @Override
+    public List<Capsule> duringGetCapsules(CapsuleRequestParameters params) {
+        JsonResponse response = RequestHandler.requestCapsules(this.getApplicationContext(),
+                this.mAccount, params);
+        return response.getCapsules();
+    }
+
+    /**
+     * Executes before the request for Capsules is made
+     */
+    @Override
+    public void onPreGetCapsules() {
+    }
+
+    /**
+     * Takes the parsed Capsules from the server and adds them to the list
+     *
+     * @param capsules The parsed collection of Capsules returned from the request
+     */
+    @Override
+    public void onPostGetCapsules(List<Capsule> capsules) {
+        // Indicate that the Capsules have stopped loading
+        this.mIsScrollLoading = false;
+
+        if (capsules != null && capsules.size() > 0) {
+            // Add the Capsules to the Adapter and internal collection
+            for (Capsule capsule : capsules) {
+                this.mCapsules.add(capsule);
+                this.mAdapter.add(capsule.getName());
+            }
+            // Notify changes
+            this.mAdapter.notifyDataSetChanged();
+            // Increment the request parameters page count for the next request
+            this.mRequestParams.incrementPage();
+        }
+    }
+
+    /**
+     * Executes if the request for Capsules is cancelled
+     */
+    @Override
+    public void onGetCapsulesCancelled() {
+    }
+
+    /**
+     * Method that executes after the SortDialogFragment's positive button is clicked
+     *
+     * @param fragment A reference to the SortDialogFragment
+     */
+    @Override
+    public void onConfirm(SortDialogFragment fragment) {
+        // Remove all previous items in the list
+        this.removeItems();
+        // Build the request parameters that were selected
+        this.mRequestParams = fragment.buildSortParameters();
+        // Request new Capsules based on the new search params
+        this.requestCapsules(this.mRequestParams);
+    }
+
+    /**
+     * Method that executes after the SortDialogFragment's negative button is clicked
+     *
+     * @param fragment A reference to the SortDialogFragment
+     */
+    @Override
+    public void onCancel(SortDialogFragment fragment) {
+    }
+
+    /**
+     * Requests Capsules based on the specified request parameters
+     *
+     * @param params The Capsule request parameters
+     */
+    public void requestCapsules(CapsuleRequestParameters params) {
+        // Set the params
+        this.mRequestParams = params;
+        // Instantiate the background task for getting the Capsules
+        GetCapsulesTask task = new GetCapsulesTask(this);
+        // Retain the Task on the retaining Fragment
+        this.mRetainingFragment.setTask(task);
+        // Execute the task on the background thread
+        task.execute(params);
+    }
+
+    /**
+     * Removes all Capsules from the internal member variable and clears the Adapter items
+     */
+    public void removeItems() {
+        // Empty the Capsule collection
+        this.mCapsules = new ArrayList<Capsule>();
+        // Check that the Adapter is set
+        if (this.mAdapter == null) {
+            return;
+        }
+        // Remove all items from the Adapter
+        this.mAdapter.clear();
+        this.mAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Attempts to get RetainedTaskFragment if its state has been retained.  Otherwise, will
+     * instantiate a new RetainedTaskFragment
+     */
+    private void setupRetainedFragment() {
+        // Attempt to get the retaining Fragment
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        this.mRetainingFragment =
+                (RetainedTaskFragment) fragmentManager.findFragmentByTag(TAG_RETAINING_FRAGMENT);
+        // If the retaining Fragment was not found, this Fragment is being created for the first time
+        if (this.mRetainingFragment == null) {
+            // Instantiate the retaining Fragment
+            this.mRetainingFragment = new RetainedTaskFragment();
+            fragmentManager.beginTransaction().add(this.mRetainingFragment, TAG_RETAINING_FRAGMENT)
+                    .commit();
+        }
+    }
+
+    /**
+     * Sets up the ListView and populates the Adapter with the specified Capsules
+     *
+     * @param capsules The initial Capsules to add to the ListView
+     */
+    private void setupListView(ArrayList<Capsule> capsules) {
+        // Get a reference to the ListView
+        ListView listView = (ListView) this.findViewById(R.id.capsule_list);
+        // Instantiate the Adapter
+        this.mAdapter = new ArrayAdapter<String>(this.getApplicationContext(),
+                R.layout.account_list_item, R.id.account_list_item_text);
+        listView.setAdapter(this.mAdapter);
+        // Add the initial items
+        if (capsules != null && capsules.size() > 0) {
+            for (Capsule capsule : capsules) {
+                this.mAdapter.add(capsule.getName());
+            }
+        }
+
+        // Set the scroll listener
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                                 int totalItemCount) {
+                // Determine if the ListView is being scrolled past the bottom item
+                if (totalItemCount > 0 && firstVisibleItem + visibleItemCount >= totalItemCount) {
+                    // Check if any Capsules are being loaded from a previous scroll
+                    if (!CapsuleListActivity.this.mIsScrollLoading) {
+                        // Set the scroll loading flag to active
+                        CapsuleListActivity.this.mIsScrollLoading = true;
+                        // Load more Capsules
+                        CapsuleListActivity.this.requestCapsules(
+                                CapsuleListActivity.this.mRequestParams);
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Sets up the Toolbar
+     */
+    private void setupToolbar() {
+        // Setup the Toolbar
         Toolbar toolbar = Widgets.createToolbar(this, this.getString(R.string.title_my_collection));
+        toolbar.inflateMenu(R.menu.capsule_list_activity);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -110,20 +325,22 @@ public class CapsuleListActivity extends FragmentActivity implements
                 CapsuleListActivity.this.finish();
             }
         });
-    }
-
-    /**
-     * Callback for when a CapsuleListFragment is not passed the required data
-     *
-     * @param capsuleListFragment The Fragment that is missing the data
-     */
-    @Override
-    public void onMissingData(CapsuleListFragment capsuleListFragment) {
-        this.getSupportFragmentManager().beginTransaction().remove(capsuleListFragment).commit();
-        this.setResult(Activity.RESULT_CANCELED);
-        this.finish();
-        Toast.makeText(this, this.getString(R.string.error_cannot_load_capsules),
-                Toast.LENGTH_SHORT).show();
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_sort:
+                        // Launch the SortDialogFragment
+                        SortDialogFragment fragment = SortDialogFragment.createInstance(
+                                CapsuleListActivity.this.mRequestParams);
+                        fragment.show(CapsuleListActivity.this.getSupportFragmentManager(),
+                                CapsuleListActivity.TAG_SORT_DIALOG);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
 }
